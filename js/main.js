d32c6f731c270cf3415911c40eb435c26f110525
Guillaume <dussartguillaume@orange.fr>
2017-07-27 20:32:28 +0200

    main.js->add object & method

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.card').hide();
+function admin(title,sentence,content){
+    this.title = title;
+    this.sentence = sentence;
+    this.content = content;
+    this.send = function(){
+        $('.title').text(this.title);
+        $('.sentence').text(this.sentence);
+        $('.content').text(this.content);
+    }
+}
+$('#button').click(function(){
+    var newadmin = new admin($('#title').val(),$('#sentence').val(),$('#content').val());
+    newadmin.send();
+    $('.card').show('slow')
+});
